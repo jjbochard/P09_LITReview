57e51348e7506cad548f62d86b9d731b8b00169d
Jérémy Bochard <jeremy.bochard@gmail.com>
2021-12-18 14:25:52 +0100

    add __str__ method for models

--- litreview/management/models.py
+++ litreview/management/models.py
@@ -12,6 +12,9 @@ class Ticket(models.Model):
     image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
@@ -23,6 +26,9 @@ class Review(models.Model):
     body = models.CharField(max_length=8192, blank=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.headline
 
 
 class UserFollows(models.Model):
